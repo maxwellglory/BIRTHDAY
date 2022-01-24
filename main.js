8fe3cafe382765d516507f85fdb61abc107a5b9d
maxwellglory <98107770+maxwellglory@users.noreply.github.com>
2022-01-24 17:40:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function tune1(){
+    document.getElementById("audio1").play();
+}
+function tune2(){
+    document.getElementById("audio2").play();
+}
+function tune3(){
+    document.getElementById("audio3").play();
+}
+function tune4(){
+    document.getElementById("audio4").play();
+}
+function tune5(){
+    document.getElementById("audio5").play();
+}
+function tune6(){
+    document.getElementById("audio6").play();
+}
+function tune7(){
+    document.getElementById("audio7").play();
+}
+function tune8(){
+    document.getElementById("audio8").play();
+}
+function mochaflip(){
+    document.getElementById("audio9").play();
+    document.getElementById("mocha").src="milk wish mocha.gif";
+}
+
